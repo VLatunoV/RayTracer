38f67cf3b8ddbf07a98e928677938ff7243f9da6
Valentin Latunov <vlatunov@gmail.com>
2017-02-18 11:11:20 +0200

    Fixed broken test

--- geometry/sphere_test.go
+++ geometry/sphere_test.go
@@ -9,16 +9,18 @@ func TestSphere_Intersect(t *testing.T) {
 	r := Ray{
 		Dir: util.Vec3{3, 1, 0},
 	}
-	s := Sphere{
-		Transform: util.GetIdentityTransform(),
-	}
-	s.Transform.Translate = util.Vec3{7, 1, 0}
-	s.Transform.Scale = util.Vec3{2, 2, 2}
+	transform := util.GetIdentityTransform()
+	transform.Translate = util.Vec3{7, 1, 0}
+	transform.Scale = util.Vec3{2, 2, 2}
+	s := &Sphere{}
+	r.Reverse(transform)
 	r.Dir.Normalize()
-	ii, ok := s.Intersect(r)
-	if !ok {
+	ii := s.Intersect(r)
+	if ii == nil {
 		t.Error("Expected ray to intersect the sphere, but it didn't.")
+		return
 	}
+	ii.Apply(transform)
 	expectedIntersection := util.Vec3{X: 5.130306154330093, Y: 1.7101020514433642, Z: 0}
 	if !util.Sub(expectedIntersection, ii.IntersectPoint).LengthSqr().IsZero() {
 		t.Errorf("Expected intersection point %+v, but was %+v", expectedIntersection, ii.IntersectPoint)
